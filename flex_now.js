f85b7b38b54357d07f6829770336cbc2fa98cd8a
naoto hamada <hamada@vis-its.com>
2019-07-12 17:11:46 +0900

    fix bug

--- flex_now.js
+++ flex_now.js
@@ -1,18 +1,13 @@
 javascript:(function(){
-    const kinm = Number($($('.infotpl tr')[7]).children('td').text())
-    const kyuka = Number($($('.infotpl tr')[9]).children('td').text())
-    const sumIndex = $('.note tr').length - 1
-    const sum = $.trim($($($('.note tr')[sumIndex]).children('td')[4]).text())
-    const ms = sum.split(':')
-    const nt = Number(ms[0]) * 60 + Number(ms[1])
-    const t = kinm * 8 * 60
-    console.log(t)
-    console.log(kinm)
-    console.log(ms)
-    console.log(nt)
-    if (t > nt) {
-        alert((t - nt) + '分、余裕があります')
+    const kinm = Number($($('.infotpl tr')[7]).children('td').text());
+    const sumIndex = $('.note tr').length - 1;
+    const sum = $.trim($($($('.note tr')[sumIndex]).children('td')[4]).text());
+    const ms = sum.split(':');
+    const nt = Number(ms[0]) * 60 + Number(ms[1]);
+    const t = kinm * 8 * 60;
+    if (t < nt) {
+        alert((nt - t) + '分、余裕があります');
     } else {
-        alert((nt - t) + '分、マイナスです')
+        alert((t - nt) + '分、マイナスです');
     }
 })()
