b41df5327664143f12c07643cc7e41e3227be0aa
naoto hamada <hamada@vis-its.com>
2020-12-17 10:25:05 +0900

    Fix flex_now

--- flex_now.js
+++ flex_now.js
@@ -1,7 +1,6 @@
 javascript:(function(){
-  const kinm = Number($($('.infotpl tr')[7]).children('td').text());
-  const sumIndex = $('.note tr').length - 1;
-  const sum = $.trim($($($('.note tr')[sumIndex]).children('td')[4]).text());
+  const kinm = Number($($('.info-contents tr')[0]).children('td').text());
+  const sum = $($('.jbc-table-footer td')[3]).text();
   const ms = sum.split(':');
   const nt = Number(ms[0]) * 60 + Number(ms[1]);
   const t = kinm * 8 * 60;
@@ -11,13 +10,18 @@ javascript:(function(){
     alert((t - nt) + '分、マイナスです');
   }
 
-  today = window.prompt("本日の時間をxx:xxで入力してください", "");
+  today = $('.jbc-table-bordered td:contains("勤務中")').next().text()
   const ms2 = today.split(':');
   if (ms2.length == 2) {
     const nt2 = nt - (Number(ms2[0]) * 60 + Number(ms2[1]));
     const t2 = (kinm - 1) * 8 * 60;
     if (t2 < nt2) {
-      alert('前日時点で' + (nt2 - t2) + '分、余裕があります');
+      const minutes = nt2 - t2;
+      if (minutes >= 60) {
+        alert('前日時点で' + (Math.round(minutes / 60, 1)) + 'h、余裕があります');
+      } else {
+        alert('前日時点で' + (nt2 - t2) + '分、余裕があります');
+      }
     } else {
       alert('前日時点で' + (t2 - nt2) + '分、マイナスです');
     }
